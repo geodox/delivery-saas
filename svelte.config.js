b7d3920d5eb2c3b698ced66dea2dbcd62e0133ca
geodox <mcdoogledude@gmail.com>
2025-07-07 13:53:45 -0400

    SvelteKit bun adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,14 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "svelte-adapter-bun-next";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter(),
+	},
 };
 
 export default config;
